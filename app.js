2a614b18c2eeccd58043dbee022501d5c3d4a302
Tanguy Coenen <tanguy.coenen@gmail.com>
2018-04-25 21:33:15 +0200

    first version of context search

--- app.js
+++ app.js
@@ -7,29 +7,40 @@ var levelOfExpertise = [];
 var duration = [];
 var info = [];
 var targetProfile = [];
+var type = [];
+var formatOfDelivery = [];
 
+//positions of the parameters in the csv
+var typePosition = 1;
 
 csv.parseCSV("courses.csv", function(data){
    courses = data;
-   mapItemsToField(3,areasOfInterest);
-   mapItemsToField(4,levelOfExpertise);
-   mapItemsToField(5,targetProfile);
-   mapItemsToField(6,duration);
+   mapItemsToFields();
    //mapItemsToField(7,info);
 },false);
 
+function mapItemsToFields(){
+  type = mapItemToField(typePosition);
+  formatOfDelivery = mapItemToField(2);
+  areasOfInterest = mapItemToField(3);
+  levelOfExpertise = mapItemToField(4);
+  targetProfile = mapItemToField(5);
+  duration = mapItemToField(6);
+}
+
 /*
-Map a column in the parsed csv to a particular array
+Map a column in the parsed csv to a particular array, not inluding duplicates
 */
-function mapItemsToField(colnr,newArray) {
-  var i=0;
+function mapItemToField(colnr) {
+  var newArray = [];
+  var i = 0;
   for(var course of courses){
     if (!newArray.includes(course[colnr])&&i>0){
       newArray.push(course[colnr]);
       }
     i=i+1;
   }
-  console.log(newArray);
+  return newArray;
 }
 
 
@@ -45,6 +56,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
     //appId: process.env.MicrosoftAppId,
     //appPassword: process.env.MicrosoftAppPassword
+
+    /*When using the emulator, use the null credentials*/
     appId: null,
     appPassword: null
 });
@@ -53,37 +66,36 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-function findCourse(area,expertise,duration) {
-  //var course = courses.find(o => o[0] === parameter);
-  var course = courses.find(o => o[3] === area && o[4] === expertise && o[5]);
-  console.log("****found course: "+course);
-  return course;
-}
-
-
-
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-/*
-var bot = new builder.UniversalBot(connector, function (session) {
-    var course = findCourse(session.message.text)
-    //session.send("You said: %s", session.message.text);
-    session.send("Found course: %s", course);
-});
-*/
-
 var inMemoryStorage = new builder.MemoryBotStorage();
 
 var bot = new builder.UniversalBot(connector);
+
+const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/8ef54f6e-1b7c-493b-b93d-45668bf93dbf?subscription-key=a1f8565cfb844254b2ff3b7076896d9a&verbose=true&timezoneOffset=60&q=';
+
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+.matches('identity', (session) => {
+    session.beginDialog('startDialog');
+})
+.matches('find_course', (session) => {
+    //session.send("Let's try to find you a course");
+    session.beginDialog('findCourseDialog');
+})
+.matches('Cancel', (session) => {
+    session.send('Do you want to stop?');
+})
+.onDefault((session) => {
+    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+});
+
+bot.dialog('/', intents);
 
 bot.dialog('startDialog', [
   function (session) {
     session.send('I am the Imec Academy bot, AIBOT for short, nice to meet you');
     builder.Prompts.choice(session, "Would you like me to help you find one of our great courses?", ["yes","no"], { listStyle: builder.ListStyle.button });
-    // if yes, then start findCourseDialog
   },
   function (session, results) {
-    console.log(results.response);
     if (results.response.entity == "yes")
         session.beginDialog('findCourseDialog');
   }
@@ -91,8 +103,18 @@ bot.dialog('startDialog', [
 
 
 bot.dialog('findCourseDialog', [
+  //todo: add search dialgoue for type and formatOfDelivery
+  //todo: make sure the choices can be made are restricted by the previous choices, so the user is more likely to find a course
   function (session) {
       session.send("Ok, let's go...");
+      console.log("*****type");
+      console.log(type);
+      builder.Prompts.choice(session, "What type of course are you interested in?", type, { listStyle: builder.ListStyle.button });
+  },
+  function (session, results) {
+      session.dialogData.type = results.response;
+      findCoursesByContext(typePosition,results.response.entity);
+      mapItemsToFields();
       builder.Prompts.choice(session, "Please provide an area of interest", areasOfInterest, { listStyle: builder.ListStyle.button });
   },
   function (session, results) {
@@ -115,7 +137,7 @@ bot.dialog('findCourseDialog', [
       if (course)
         session.send('We found the following couse for you:'+course);
       else {
-
+        session.send("I'm very sorry, but I found no course that matches your needs");
       }
       session.endDialog();
   }
@@ -129,24 +151,16 @@ bot.dialog('findCourseDialog', [
         matches: /^help$/i,
     });
 
+function findCourse(area,expertise,duration) {
+  var course = courses.find(o => o[3] === area && o[4] === expertise && o[5]);
+  return course;
+}
 
-
-const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/8ef54f6e-1b7c-493b-b93d-45668bf93dbf?subscription-key=a1f8565cfb844254b2ff3b7076896d9a&verbose=true&timezoneOffset=60&q=';
-
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('identity', (session) => {
-    session.beginDialog('startDialog');
-})
-.matches('find_course', (session) => {
-    //session.send("Let's try to find you a course");
-    session.beginDialog('findCourseDialog');
-})
-.matches('Cancel', (session) => {
-    session.send('Do you want to stop?');
-})
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-});
-
-bot.dialog('/', intents);
+function findCoursesByContext(searchIndex,searchTerm) {
+  //send a number of parameters and find the courses that match, to be able to show a limited set of choice options
+  //substitute the courses global variable by a smaller subset
+  //working with global vars in this way is bad practice, shuold be refactored
+  console.log("*************search term= "+searchTerm);
+  courses = courses.filter(o => o[searchIndex] === searchTerm);
+  console.log(courses);
+}
